b582dd4438d8792fab5579dd7d904d4481efc29e
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2022-08-17 10:13:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
-let age = 26
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
 
-// less than 6 years old -> free
-// 6 to 17 years old     -> child discount
-// 18 to 26 years old    -> student discount
-// 27 to 66 years old    -> full price
-// over 66 years old     -> senior citizen discount
-
-// Create a conditional statement (if/else/else if) that logs out the discount
-// the passenger will get based upon the value of the age variable
-
-if (age < 6) {
-  console.log('Free')
-} else if (age < 18) {
-  console.log('Child Discount')
-} else if (age < 27) {
-  console.log('Student Discount')
-} else if (age < 67) {
-  console.log('full price')
-} else {
-  console.log('senior citizen discount')
+console.log("The 5 largest countries in the world:")
+for (let i = 0; i < largeCountries.length; i++) {
+  console.log("-" + largeCountries[i]);
 }
